7e71f1a992ff381a948d671cbdd85369780fa2e4
yasmalhotraa <yashmalhotra330@gmail.com>
2026-04-16 12:57:18 +0530

    fix bug for jwt import

--- src/middlewares/auth.middleware.js
+++ src/middlewares/auth.middleware.js
@@ -1,4 +1,4 @@
-import { jwt } from "jsonwebtoken";
+import jwt from "jsonwebtoken";
 import { ApiError } from "../utils/ApiError.js";
 import { asyncHandler } from "../utils/asyncHandler.js";
 import { User } from "../models/user.model.js";
