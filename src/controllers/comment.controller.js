f2a3c933522b24be85ed8869e84c486375211ccd
Yash Malhotra <yashmalhotra330@gmail.com>
2026-04-28 00:45:42 +0530

    implement comment controller to add, update and delete comment

--- src/controllers/comment.controller.js
+++ src/controllers/comment.controller.js
@@ -1,8 +1,10 @@
-import mongoose from "mongoose";
+import mongoose, { isValidObjectId } from "mongoose";
 import { Comment } from "../models/comment.model.js";
 import { ApiError } from "../utils/ApiError.js";
 import { ApiResponse } from "../utils/ApiResponse.js";
 import { asyncHandler } from "../utils/asyncHandler.js";
+import { Video } from "../models/video.model.js";
+import { Like } from "../models/like.model.js";
 
 const getVideoComments = asyncHandler(async (req, res) => {
   //TODO: get all comments for a video
@@ -12,14 +14,98 @@ const getVideoComments = asyncHandler(async (req, res) => {
 
 const addComment = asyncHandler(async (req, res) => {
   // TODO: add a comment to a video
+  const { videoId } = req.params;
+  const { content } = req.body;
+
+  if (!isValidObjectId(videoId)) {
+    throw new ApiError(400, "Video id is not valid");
+  }
+
+  if (!content || content.trim() === "") {
+    throw new ApiError(400, "Content is required");
+  }
+
+  const video = await Video.findById(videoId);
+  if (!video) {
+    throw new ApiError(404, "Video not found");
+  }
+
+  const comment = await Comment.create({
+    content,
+    video: videoId,
+    owner: req.user?._id,
+  });
+
+  if (!comment) {
+    throw new ApiError(500, "Could not add comment");
+  }
+
+  res
+    .status(201)
+    .json(new ApiResponse(201, comment, "Comment added successfully"));
 });
 
 const updateComment = asyncHandler(async (req, res) => {
   // TODO: update a comment
+  const { commentId } = req.params;
+  const { content: updatedContent } = req.body;
+
+  if (!updatedContent || updatedContent.trim() === "") {
+    throw new ApiError(400, "Content is required");
+  }
+
+  if (!isValidObjectId(commentId)) {
+    throw new ApiError(400, "Comment id is not valid");
+  }
+
+  const updatedComment = await Comment.findOneAndUpdate(
+    {
+      _id: commentId,
+      owner: req.user?._id,
+    },
+    {
+      content: updatedContent.trim(),
+    },
+    {
+      new: true,
+      runValidators: true,
+    }
+  );
+
+  if (!updatedComment) {
+    throw new ApiError(404, "Comment not found or access denied");
+  }
+
+  res
+    .status(200)
+    .json(new ApiResponse(200, updatedComment, "Comment updated successfully"));
 });
 
 const deleteComment = asyncHandler(async (req, res) => {
   // TODO: delete a comment
+  const { commentId } = req.params;
+
+  if (!isValidObjectId(commentId)) {
+    throw new ApiError(400, "Comment id is not valid");
+  }
+
+  const deleteResponse = await Comment.findOneAndDelete({
+    _id: commentId,
+    owner: req.user?._id,
+  });
+
+  if (!deleteResponse) {
+    throw new ApiError(404, "Comment not found or access denied");
+  }
+
+  // delete likes under that comment too
+  await Like.deleteMany({
+    comment: commentId,
+  });
+
+  return res
+    .status(200)
+    .json(new ApiResponse(200, commentId, "Comment deleted successfully"));
 });
 
 export { getVideoComments, addComment, updateComment, deleteComment };
