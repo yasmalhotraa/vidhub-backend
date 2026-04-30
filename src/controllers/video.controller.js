6a1555f9e672a9190890d392e0eae91decf504af
yasmalhotraa <yashmalhotra330@gmail.com>
2026-04-30 11:48:57 +0530

    implement video update functionality with title, description and thumbnail upload and deletion

--- src/controllers/video.controller.js
+++ src/controllers/video.controller.js
@@ -4,7 +4,10 @@ import { User } from "../models/user.model.js";
 import { ApiError } from "../utils/ApiError.js";
 import { ApiResponse } from "../utils/ApiResponse.js";
 import { asyncHandler } from "../utils/asyncHandler.js";
-import { uploadOnCloudinary } from "../utils/cloudinary.js";
+import {
+  deleteFromCloudinary,
+  uploadOnCloudinary,
+} from "../utils/cloudinary.js";
 
 const getAllVideos = asyncHandler(async (req, res) => {
   const { page = 1, limit = 10, query, sortBy, sortType, userId } = req.query;
@@ -66,6 +69,76 @@ const getVideoById = asyncHandler(async (req, res) => {
 const updateVideo = asyncHandler(async (req, res) => {
   const { videoId } = req.params;
   //TODO: update video details like title, description, thumbnail
+  const { title, description } = req.body;
+
+  if (!isValidObjectId(videoId)) {
+    throw new ApiError(400, "Invalid Video Id");
+  }
+
+  if ([title, description].some((field) => field.trim() === "")) {
+    throw new ApiError(400, "Title and Description are required");
+  }
+
+  const thumbnailLocalPath = req.file?.path;
+
+  if (!thumbnailLocalPath) {
+    throw new ApiError(400, "Thumbnail is required");
+  }
+
+  const existingVideo = await Video.findOne({
+    _id: videoId,
+    owner: req.user?._id,
+  }).select("thumbnail.public_id");
+
+  if (!existingVideo) {
+    throw new ApiError(404, "Video not found or unauthorized");
+  }
+
+  const oldThumbnailId = existingVideo?.thumbnail?.public_id;
+
+  const thumbnail = await uploadOnCloudinary(thumbnailLocalPath);
+
+  if (!thumbnail?.url) {
+    throw new ApiError(400, "Something went wrong while uploading thumbnail");
+  }
+
+  const updatedVideo = await Video.findOneAndUpdate(
+    {
+      _id: videoId,
+      owner: req.user?._id,
+    },
+    {
+      $set: {
+        title: title.trim(),
+        description: description.trim(),
+        thumbnail: {
+          url: thumbnail.url,
+          public_id: thumbnail.public_id,
+        },
+      },
+    },
+    {
+      new: true,
+    }
+  );
+
+  if (!updatedVideo) {
+    await deleteFromCloudinary(thumbnail.public_id);
+
+    throw new ApiError(404, "Video not found or unauthorized");
+  }
+
+  try {
+    if (oldThumbnailId) {
+      await deleteFromCloudinary(oldThumbnailId);
+    }
+  } catch (error) {
+    console.log(`Failed to delete old thumbnail ${error}`);
+  }
+
+  res
+    .status(200)
+    .json(new ApiResponse(200, updatedVideo, "Video updated successfully"));
 });
 
 const deleteVideo = asyncHandler(async (req, res) => {
